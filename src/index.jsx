c9476a3dd98e3dc4059f3ba63791ceb45c034e89
HsuTing <hsuting0106@gmail.com>
2016-02-07 21:35:30 +0800

    add about me page

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,10 @@
 'use strict';
 
 (() => {
-});
+  let initInterval = setInterval(() => {
+    if(document.querySelector('.mdl-layout__container') != null) {
+      clearInterval(initInterval);
+      document.querySelector('body').className += ' body__show';
+    }
+  }, 10);
+})();

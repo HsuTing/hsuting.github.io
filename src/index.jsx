bc2318cfe39861a2e3110dbd802afe210747ae4a
HsuTing <hsuting0106@gmail.com>
2016-02-15 13:44:04 +0800

    change to data

--- src/index.jsx
+++ src/index.jsx
@@ -3,16 +3,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import GetJson from './get_json';
+
 import AboutMeData from './about_me_component/about_me_data';
 import AboutMe from './about_me';
 
 import SkillData from './skill_component/skill_data';
 import Skill from './skill';
 
-import WorkData from './work_component/work_data';
 import Work from './work';
 
-import RecentDevelopmentsData from './recent_developments_component/recent_developments_data';
 import RecentDevelopments from './recent_developments';
 
 import ContactData from './contact_component/contact_data';
@@ -21,7 +21,18 @@ import Contact from './contact';
 (() => {
   ReactDOM.render(<AboutMe data={AboutMeData} />, document.getElementById('about-me'));
   ReactDOM.render(<Skill data={SkillData} />, document.getElementById('skill'));
-  ReactDOM.render(<Work data={WorkData} />, document.getElementById('work'));
-  ReactDOM.render(<RecentDevelopments data={RecentDevelopmentsData} />, document.getElementById('recent-developments'));
+
+  GetJson('./data/work_data.json', (data) => {  
+    ReactDOM.render(<Work data={data} />, document.getElementById('work'));
+  }, () => {
+    alert('You can not connect to server.');
+  });
+
+  GetJson('./data/recent_developments_data.json', (data) => {  
+    ReactDOM.render(<RecentDevelopments data={data} />, document.getElementById('recent-developments'));
+  }, () => {
+    alert('You can not connect to server.');
+  });
+
   ReactDOM.render(<Contact data={ContactData} />, document.getElementById('contact'));
 })();

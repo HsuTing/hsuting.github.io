dc5de23045cef2158cd4b5e1c2cb181817833142
HsuTing <hsuting0106@gmail.com>
2017-10-10 14:40:58 +0800

    add github

--- src/components/Github.js
+++ src/components/Github.js
@@ -2,8 +2,14 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
-import radium from 'radium';
+import radium, {StyleRoot} from 'radium';
+import fetch from 'fetch-everywhere';
 import {language} from 'cat-components/lib/i18n';
+
+import {
+  title as titleStyle,
+  content as contentStyle
+} from 'componentsShare/style/style';
 
 import * as style from './style/github';
 
@@ -14,20 +20,57 @@ export default class Gtihub extends React.Component {
     translate: PropTypes.object.isRequired
   }
 
-  shouldComponentUpdate(nextProps) {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: null,
+      description: null,
+      link: null
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://api.github.com/users/HsuTing/repos?sort=pushed')
+      .then(res => res.json())
+      .then(data => {
+        const {name, description, html_url: link} = data[0];
+
+        this.setState({name, description, link});
+      });
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
     return (
-      JSON.stringify(this.props.translate) !== JSON.stringify(nextProps.translate)
+      JSON.stringify(this.props.translate) !== JSON.stringify(nextProps.translate) ||
+      this.state.name !== nextState.name
     );
   }
 
   render() {
     const {translate} = this.props;
+    const {name, description, link} = this.state;
     const {github} = translate;
 
     return (
-      <div style={style.root}>
-        {github}
-      </div>
+      <StyleRoot style={style.root}>
+        <StyleRoot style={style.container}>
+          <div style={titleStyle}
+          >{github}</div>
+
+          <div style={contentStyle}>
+            {description}
+
+            {
+              !name ?
+                null :
+                <a style={style.projectName}
+                  href={link}
+                  target='_blank'
+                >{name}. </a>
+            }
+          </div>
+        </StyleRoot>
+      </StyleRoot>
     );
   }
 }
--- src/components/style/github.js
+++ src/components/style/github.js
@@ -2,8 +2,39 @@
 
 import black from 'cat-components/lib/color/black';
 import white from 'cat-components/lib/color/white';
+import * as layoutStyle from 'cat-components/lib/layout';
 
 export const root = {
   color: white,
-  background: black
+  background: black,
+  padding: '50px 100px',
+  ...layoutStyle.tablet({
+    padding: '50px 60px'
+  }),
+  ...layoutStyle.phone({
+    padding: '50px 20px'
+  })
+};
+
+export const container = {
+  margin: 'auto',
+  padding: '50px',
+  width: 'calc(100% - 50px * 2)',
+  maxWidth: '600px',
+  textAlign: 'center',
+  border: `0.5px solid ${white}`,
+  ...layoutStyle.phone({
+    border: '0px'
+  })
+};
+
+export const projectName = {
+  display: 'block',
+  margin: '20px auto 0px',
+  padding: '10px 5px',
+  width: '100%',
+  maxWidth: '250px',
+  fontSize: '14px',
+  fontWeight: 'medium',
+  border: `0.5px solid ${white}`
 };
